c84383a0d7767b0e351a2f68f38e96450aabb135
Jonas Jonson <boiejonson@gmail.com>
2016-12-10 16:54:29 +0100

    Created Project Started Creating BOs

---
+++ PostureRepo/ExerciseBO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostureRepo
+{
+    public class ExerciseBO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<object> Pictures { get; set; }
+        public int DefaultSets { get; set; }
+        public int DefaultReps { get; set; }
+    }
+}
---
+++ PostureRepo/WorkoutBO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostureRepo
+{
+    class WorkoutBO
+    {
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+        public List<ExerciseBO> Excercises{ get; set; }
+    }
+}
