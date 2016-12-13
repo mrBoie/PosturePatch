59227704d1ef91405c30de7fcdbab4d680b36067
Jonas Jonson <boiejonson@gmail.com>
2016-12-13 23:14:40 +0100

    Just playing around...

---
+++ PostureRepo/Create Script/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Client]
+(
+	[ID] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NULL, 
+    [Age] TINYINT NULL, 
+    [Gender] TINYINT NULL, 
+    [Description] VARCHAR(5000) NULL
+)
